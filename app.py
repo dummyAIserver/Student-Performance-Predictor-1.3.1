babbfa68dd89e19b7b2d9d5d5d5d76b4d0d32b84
dummyAIserver <dummypm753@gmail.com>
2026-05-15 22:58:00 +0530

    Refactor database creation to app context
    Moved db.create_all() to the application context during startup.

--- app.py
+++ app.py
@@ -345,8 +345,8 @@ def api_analytics():
 # Register main blueprint
 app.register_blueprint(main_bp)
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
